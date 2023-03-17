3a85fa41342978c42d8193fb2fbd129fdb02df12
Eduard Karol <korol94.94@gmail.com>
2023-03-17 23:35:15 +0300

    add settings.py and replace game screen resolution with background, add frame_rate 60, added image of ship and load to custom class Ship on another file + select start position of ship, add ship on main screen, with smaller resolution

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
 
 import pygame
+from settings import Settings
+from ship import Ship
 
 class AlienInvesion:
     '''Overall class to manage game assets and behavior.'''
@@ -8,9 +10,13 @@ class AlienInvesion:
     def __init__(self):
         """Initialize the game, and a create game resources."""
         pygame.init()
-
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
+        self.ship = Ship(self)
+        # Set the background color.
+        # self.bg_color = (230, 230, 230)
 
     def run_game(self):
         '''Start the main loop for the game.'''
@@ -19,9 +25,12 @@ class AlienInvesion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
